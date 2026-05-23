24b1da4689fcd1f5ba5dcc82a37319c65cc46a8a
packetFury <packetfury@pm.me>
2026-05-23 06:56:14 -0500

    Fixed execution plan to prevent timeouts.

--- main.tf
+++ main.tf
@@ -1,47 +1,48 @@
 terraform {
-    required_providers {
-        proxmox = {
-            source = "telmate/proxmox"
-            version = "3.0.2-rc07"
-        }
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.2-rc07"
     }
+  }
 }
 
 provider "proxmox" {
-    pm_api_url          = var.proxmox_api_url
-    pm_api_token_id     = var.proxmox_api_token_id
-    pm_api_token_secret = var.proxmox_api_token_secret
-    pm_tls_insecure     = true
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_tls_insecure     = true
+  pm_timeout          = 3600
 }
 
 resource "proxmox_vm_qemu" "vm-instance" {
-    name                = "vm-instance"
-    target_node         = "azuris"
-    clone               = "template-debian-13"
-    full_clone          = true
-    memory              = 8192
+  name        = "vm-instance"
+  target_node = "azuris"
+  clone       = "template-debian-13"
+  full_clone  = true
+  memory      = 8192
 
-    cpu {
-	cores		= 2
-    }
+  cpu {
+    cores = 2
+  }
 
-    disk {
-	slot		= "scsi0"
-        size            = "100G"
-        type            = "disk"
-        storage         = "ds03"
-        discard         = "false"
-    }
+  disk {
+    slot    = "scsi0"
+    size    = "100G"
+    type    = "disk"
+    storage = "ds03"
+    discard = "false"
+  }
 
-    network {
-	id	  = 0
-        model     = "virtio"
-        bridge    = "vmbr0"
-        firewall  = false
-        link_down = false
-    }
-    timeouts {
-	create = "60m"
-	delete = "10m"
-    }
+  network {
+    id        = 0
+    model     = "virtio"
+    bridge    = "vmbr0"
+    firewall  = false
+    link_down = false
+  }
+  timeouts {
+    create = "60m"
+    delete = "10m"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "proxmox_api_url" {
-    type	= string
-    description	= "The API endpoint for the Proxmox cluster"
+  type        = string
+  description = "The API endpoint for the Proxmox cluster"
 }
 
 variable "proxmox_api_token_id" {
-    type	= string
-    description	= "The Proxmox Token ID"
+  type        = string
+  description = "The Proxmox Token ID"
 }
 
 variable "proxmox_api_token_secret" {
-    type	= string
-    sensitive	= true
-    description	= "The secret key associated with the Proxmox API token"
+  type        = string
+  sensitive   = true
+  description = "The secret key associated with the Proxmox API token"
 }
